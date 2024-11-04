8acc810e59f38e343546715722169f44b2f5390c
maoif <maoif@foxmail.com>
2024-11-04 11:49:32 +0800

    perf_func:
    1) use newer report,
    2) use total time to compare performance.

--- perf_func.py
+++ perf_func.py
@@ -31,16 +31,84 @@ from perflib import *
 # draw diagram
 
 
-def outputDiagram():
-    pass
+def dedup_reports(results: list[PerfResult]):
+    res: dict[str, PerfResult] = {}
+    for r in results:
+        if r.func not in res.keys():
+            res[r.func] = r
+
+    return list(res.values())
 
 
-def outputExcel():
-    pass
+class ReportItemNew:
+    def __init__(self, func: str, fid1, fid2, code: str, file: str, plot_id: int, ratio):
+        self.func = func
+        self.fid1 = fid1
+        self.fid2 = fid2
+        self.code = code
+        self.file = file
+        self.plot_id = plot_id
+        self.ratio = round(ratio*100, 2)
 
 
-def outputText(data, interval):
-    pass
+class FuncPlot:
+    def __init__(self, plot_id, interval, dist1: list[int], dist2: list[int]):
+        self.plot_id = plot_id
+        self.interval = interval
+
+        self.xs1= []
+        self.xs2= []
+        self.ys1= []
+        self.ys2= []
+
+        t = 0
+        for c in dist1:
+            self.ys1.append(c)
+            self.xs1.append(t)
+            t += 1
+        t = 0
+        for c in dist2:
+            self.ys2.append(c)
+            self.xs2.append(t)
+            t += 1
+
+
+def generate_report_new(results: list[PerfResult], path = '.'):
+    results = dedup_reports(results)
+    print(f'Generating report for {len(results)} results...')
+    reports: ReportItemNew = []
+    plots = []
+
+    plot_id = 0
+    for res in results:
+        srcs, src_file = fetch_source_code(res)
+        # print(res.func)
+        ss = ''
+        for s in srcs:
+            ss += s
+        reports.append(ReportItemNew(res.func, res.fid1, res.fid2, ss, src_file, plot_id, res.ratio))
+        plots.append(FuncPlot(plot_id, res.pd1.interval, res.dist1, res.dist2))
+
+        plot_id += 1
+
+    if reports == []:
+        return
+
+    filename = Path(reports[0].file).parts[0]
+
+    from jinja2 import Environment, PackageLoader, select_autoescape
+    env = Environment(
+        loader=PackageLoader("perflib"),
+        autoescape=select_autoescape()
+    )
+    template = env.get_template('report_new.html')
+    with open(f'{filename}.html', 'w') as f:
+        print('Rendering report...')
+        f.write(template.render(perf_package = filename,
+            interval = results[0].pd1.interval, buckets = results[0].pd1.buckets,
+            arch1 = results[0].pd1.arch.name, arch2 = results[0].pd2.arch.name,
+            reports = reports, plots = plots))
+    print('Rendered.')
 
 
 def main(dir1: str, dir2: str):
@@ -87,7 +155,7 @@ def main(dir1: str, dir2: str):
         pd.srcDir = srcDir2
 
     res, good_res = analyze_all(perfDatas1, perfDatas2)
-    generate_report(res)
+    generate_report_new(res)
 
 
 ###
@@ -100,7 +168,11 @@ if __name__ == '__main__':
     parser.add_argument('dataDir1', type=str, help='directory of perf data and debuginfo from the 1st archtecture')
     parser.add_argument('dataDir2', type=str, help='directory of perf data and debuginfo from the 2nd archtecture')
     parser.add_argument('-p', '--prefix', type=str, help='path prefix inside OBS environemnt')
+    parser.add_argument('-t', '--threshold', type=str, help='bad performance threshold, default: 0.5')
 
     args = parser.parse_args()
     if not args.prefix == None:
         set_g_obs_prefix(args.prefix)
+    if not args.threshold == None:
+        set_g_bad_threshold(args.threshold)
+    main(args.dataDir1, args.dataDir2)
